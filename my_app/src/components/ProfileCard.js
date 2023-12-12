60aef340e63aa8244e38f00065af6a912e962f4b
Gaston Saucedo <gastonsaucedo2001@gmail.com>
2023-12-11 21:59:12 -0300

    boton sumar y restar agregado

--- my_app/src/components/ProfileCard.js
+++ my_app/src/components/ProfileCard.js
@@ -1,9 +1,22 @@
-
+import { useState } from "react"
 
 
 function ProfileCard(props) {
 
     const { titulo, precio, img } = props
+
+    const [count, setCount] = useState(0)
+
+
+    function handleClickSuma() {
+        setCount (count + 1)
+    }
+    function handleClickResta() {
+        if (count > 0 ) {
+            setCount (count - 1)
+        }
+        
+    }
 
     return (
         <div className="card">
@@ -17,6 +30,20 @@ function ProfileCard(props) {
                 <div className="media-content">
                     <h3 className="title is-6">{titulo}</h3>
                     <p className="subtitle is-7">{precio}</p>
+                </div>
+                <div className="card-footer columns">
+                    <div className="column">
+                    <button onClick={handleClickResta} className="button is-light is-fullwidth">-</button>
+                    </div>
+                    <div className="column">
+                    <p className="">{count}</p>
+                    </div>
+                    <div className="column">
+                    <button onClick={handleClickSuma} className="button is-light is-fullwidth">+</button>
+                    </div>
+                    
+                    
+                    
                 </div>
             </div>
         </div>
